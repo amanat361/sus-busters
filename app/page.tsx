31d79676499c6c6d3f56b599c4502baa9c89f759
amanat361 <amanat361@gmail.com>
2024-06-22 15:31:26 -0700

    added components to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/primitives/button";
+import { Heading } from "@/components/primitives/heading";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>
-        Sus Busters...Cumming soon...
-      </h1>
+      <Heading>Sus Busters...Cumming soon...</Heading>
+      <Button>Get Started</Button>
     </main>
   );
 }
